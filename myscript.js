7e129c400feddf40a0bf8ebd69569f418938a4cc
antoniysh <78683084+antoniysh@users.noreply.github.com>
2021-11-18 01:24:39 +0200

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,40 +1,37 @@
 let calculateBtn = document.getElementById("calculate")
+let returnDiv = document.getElementById("cwev")
 let matrixG = 0
 calculateBtn.addEventListener("click",()=>{
 	try{
 		tbl = document.getElementById("tbl")
 		tbl.remove()
-	}catch(qq){
-		console.log("asd")
-
-	}
-	document.getElementById("cwev").innerHTML = ""
-	matrixG = document.getElementById("g").value
-	matrixU = document.getElementById("u").value
+	}catch(qq){}
+	returnDiv.innerHTML = ""
+	let matrixG = document.getElementById("g").value
+	let matrixU = document.getElementById("u").value
 	let g = inputToMatrix(matrixG)
 	let u = inputToMatrix(matrixU)
 	validate(g,u)
 	u.forEach((el, i, self)=>{
 		self[i] = self[i].join('')
 	})
-	console.log(u)
 	let words = getCodeWords(g)
 	let table = findError(words)
 	tableToString(table)
 	createTable(table)
-	document.getElementById("cwev").innerHTML += "Кодови думи: ["
+	returnDiv.innerHTML += "Кодови думи: ["
 	table[0].forEach(word =>{
-		document.getElementById("cwev").innerHTML += word
-		document.getElementById("cwev").innerHTML += ", "
+		returnDiv.innerHTML += word
+		returnDiv.innerHTML += ", "
 	})
-	document.getElementById("cwev").innerHTML += "] <br><br>"
-	document.getElementById("cwev").innerHTML += "Вектори на грешката: ["
+	returnDiv.innerHTML += "] <br><br>"
+	returnDiv.innerHTML += "Вектори на грешката: ["
 	table.forEach((el,i,self)=> {
 		if(i != 0){
-		document.getElementById("cwev").innerHTML += self[i][0]
-		document.getElementById("cwev").innerHTML += ", "}
+		returnDiv.innerHTML += self[i][0]
+		returnDiv.innerHTML += ", "}
 	})
-	document.getElementById("cwev").innerHTML += "]<br><br>"
+	returnDiv.innerHTML += "]<br><br>"
 
 	u.forEach((el,i,self)=>{
 		let check = 0
@@ -42,50 +39,48 @@ calculateBtn.addEventListener("click",()=>{
 			for(let k = 1; k < table[j].length; k++){
 				if(self[i].localeCompare(table[j][k])==0){
 					check = 1
-					document.getElementById("cwev").innerHTML += self[i]
-					document.getElementById("cwev").innerHTML += " открита с грешка: "
-					document.getElementById("cwev").innerHTML += table[j][0]
-					document.getElementById("cwev").innerHTML += "<br>"
+					returnDiv.innerHTML += (self[i] + " - изпратена дума: " + getSendWord(self[i],table[j][0]))
+					returnDiv.innerHTML += " с грешка: "
+					returnDiv.innerHTML += table[j][0]
+					returnDiv.innerHTML += "<br>"
 				}
 
 			}
 		}
 		if(check == 0){
-			document.getElementById("cwev").innerHTML += self[i]
-			document.getElementById("cwev").innerHTML += " думата не е открита <br>"
+			returnDiv.innerHTML += self[i]
+			returnDiv.innerHTML += " - думата не е открита <br>"
 
 		}
-	})
-
-
-	
+	})	
 })
 
 
-function getK(arr){
+let getK = function(arr){
 	return arr.length
 }
-function getN(arr){
+
+let getN = function(arr){
 	return arr[1].length
 }
 
-function inputToMatrix(matrix){
+let inputToMatrix = function(matrix){
 	let arr = matrix.split("\n")
 	let arr1 = []
-	let arr2 = []
-	let i = 0;
-	arr.forEach(item =>{
+	arr.forEach((item, i) =>{
 			arr1[i] = item.split(/\s*[\s,]\s*/)
 		i++
 	})
+	console.log(arr1)
 	arr1.forEach(item =>{
-		item.forEach( (item, i, self) => self[i] = Number(item ))
+		item.forEach( (item, i, self) => self[i] = Number(item))
 	})
+	console.log(arr1)
 
 	return arr1
 }
 
-function getCodeWords(arr){
+let getCodeWords = function (arr){
 	let words = []
 	let zeroWord = []
 	let temp = []
@@ -98,7 +93,6 @@ function getCodeWords(arr){
 	arr.forEach(item =>{
 		words.push(item)
 	})
-	/////////////////////////////////////////
 	combinations = getCombinations(arr)
 	combinations = combine(combinations,2)
 	combinations.forEach(combination=>{
@@ -112,10 +106,9 @@ function getCodeWords(arr){
 	return words
 }
 
-function BinaryAdditionSimple(arr1,arr2){
+let BinaryAdditionSimple = function(arr1,arr2){
 	let answer = []
 	let length = arr1.length - 1
-	//console.log(length)
 	for(let i = 0; i <= length; i++ ){
 		if(arr1[i] == 0 && arr2[i] == 0) answer[i] = 0;
 		if(arr1[i] == 1 && arr2[i] == 0) answer[i] = 1;
@@ -124,7 +117,7 @@ function BinaryAdditionSimple(arr1,arr2){
 	}
 	return answer
 }
-function BinaryAdditionComplex(arr){
+let BinaryAdditionComplex = function(arr){
 	let answer = []
 	let temp = zeroWord(arr[0].length)
 	arr.forEach(item =>{
@@ -133,11 +126,12 @@ function BinaryAdditionComplex(arr){
 	return temp
 }
 
-function stringToInt(num){
+
+let stringToInt = function(num){
 	num = Number(num)
 }
 
-function getCombinations(arr){
+let getCombinations = function(arr){
 	let combinations = []
 	for(let i = 0; i < getK(arr);i++){
 		combinations.push(i);
@@ -145,7 +139,7 @@ function getCombinations(arr){
 	return combinations
 }
 
-var combine = function(a, min) {
+let combine = function(a, min) {
     var fn = function(n, src, got, all) {
         if (n == 0) {
             if (got.length > 0) {
@@ -166,17 +160,16 @@ var combine = function(a, min) {
     return all;
 }
 
-var zeroWord = function(x){
+let zeroWord = function(x){
 	let z = [];
 	for(let i = 0; i <= x; i++){z.push(0)}
 	return z;
 }
 
-var findError = function(words){
+let findError = function(words){
 	let w  = 0
 	let d = 99999
 	let errorVectors = []
-	//let table = [words]
 	let errorCombinations = []
 	words.forEach(word => {
 		word.forEach(symbol =>{
@@ -191,12 +184,12 @@ var findError = function(words){
 	if(t % 1 != 0) {
 		t = Math.floor(t)
 	}
+	returnDiv.innerHTML += ("Брой грешки, които кодът поправя: " + t +"<br><br>")
 	let errorVectorLength = words[0].length - 1
 	for(let i = 0; i <= errorVectorLength; i++){
 		errorCombinations.push(i)
 	}
 	err = combine(errorCombinations,1);
-	//console.log(err)
 	for(let i = err.length - 1; i >= 0; i--){
 		if (err[i].length > t){
 			err.pop()
@@ -206,7 +199,6 @@ var findError = function(words){
 	for(let i = 0; i < err.length; i++){
 		errorVectors.push(zeroWord(errorVectorLength))
 	}
-	//console.log(errorVectors.length," ", err[0].length)
 
 	errorVectors.forEach((item,i,self) => {
 		err[i].forEach(el =>{
@@ -223,45 +215,23 @@ var findError = function(words){
 		table[i].push(errorVectors[i-1])
 	}
 	
-	/*for(let i = 1; i < table.length; i++){
-		for(j = 1; j < table[0].length; j++){
-			temp = BinaryAdditionSimple(table[i][0],table[0][j])
-			table.push(temp)
-			console.log(table[i][0].length,"|",table[0][j].length)
-		}
-	}
-
-	table[1].forEach((item,i,self)=>{
-		table[1][i] = BinaryAdditionSimple[table[1][0],table[0][i]]
-	})*/
-	//console.log(errorVectors[1],"|",words[1])
-	//console.log(BinaryAdditionSimple(words[1],errorVectors[1]))
 	for(let i = 1; i < table.length; i++){
 		for(let j = 1; j < words.length; j++){
 			table[i][j] = BinaryAdditionSimple(errorVectors[i-1],words[j])
 		}
 	}
-	//table.forEach(item=>{console.log(item)})
-	//console.log(table)
 	return table
-
-
-	//console.log(table)
-	//console.log(words)
-
-
 }
-function tableToString(table){
+
+let tableToString = function(table){
 	for(i = 0; i < table.length; i++){
 		for(j = 0; j < table[i].length; j++){
 			table[i][j] = table[i][j].join('')
-			console.log(table[i][j])
 		}
 	}
-	console.log(table)
 }
 
-function createTable(tableData) {
+let createTable = function (tableData) {
   var table = document.createElement('table');
   table.setAttribute("id","tbl")
   var tableBody = document.createElement('tbody');
@@ -274,23 +244,32 @@ function createTable(tableData) {
       cell.appendChild(document.createTextNode(cellData));
       row.appendChild(cell);
     });
-
     tableBody.appendChild(row);
   });
-
   table.appendChild(tableBody);
   document.body.appendChild(table);
 }
 
 let validate = function(arr1,arr2){
 	arr1.forEach(el=>{
-		if(el.length != arr1[0].length){
-			alert("Невалидни данни")
-		}
+		if(el.length != arr1[0].length) alert("Дължините на редовете в матрицата G се различават.");
 	})
 	arr2.forEach(el=>{
-		if(el.length != arr2[0].length){
-			alert("Невалидни дани")
-		}
+		if(el.length != arr2[0].length) alert("Дължините на думите U се различават.");
 	})
+	if (arr2.length > 1 ){
+	arr1.forEach((el,i) =>{
+		if(el.length != arr2[i].length) alert("Дължината на думите U не отговаря на матрицата G.");
+	})}else{
+		if(arr2[0].length != arr1[0].length) alert("Дължината на думите U не отговаря на матрицата G.");
+	}
+}
+
+let getSendWord = function(word, err){
+	arrayWord = word.split("")
+	errAray = err.split("")
+	let answer = BinaryAdditionSimple(word,err)
+	answer = answer.join('')
+	return answer
+
 }
